819de395903e88f0861d9e0d474a79a47a13be40
jeluard <jeluard@users.noreply.github.com>
2025-05-07 13:34:35 +0200

    chore: use pallas to fetch blocks
    Signed-off-by: jeluard <jeluard@users.noreply.github.com>

--- provers/risc0/methods/guest/src/main.rs
+++ provers/risc0/methods/guest/src/main.rs
@@ -5,23 +5,17 @@ use amaru_ledger::{context, rules::{self, parse_block}};
 use risc0_zkvm::guest::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO: Implement your guest code here
+    let raw_block: Vec<u8> = env::read();
 
-    // read the input
-    let input: String = env::read();
-
-    let raw_block = hex::decode(input.clone())?;
     let block = parse_block(&raw_block[..])?;
-
-    let inputs = BTreeMap::new();
-    let _volatile_state = rules::validate_block(
+    let inputs = BTreeMap::new(); 
+    let validity = rules::validate_block(
         context::DefaultValidationContext::new(inputs),
         ProtocolParameters::default(),
         block,
     );
 
-    // write public output to the journal
-    env::commit(&input);
+    env::commit(&"");
 
     Ok(())
 }
